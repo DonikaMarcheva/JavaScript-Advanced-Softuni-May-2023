6a2b76be27de6244db3b4e2b308d846e4155bd52
Donika <marchevadonika@gmail.com>
2023-04-11 15:55:11 +0300

    .

--- Syntax, Functions and Statements - Lab/06.sumOfNumbersN…M.js
+++ Syntax, Functions and Statements - Lab/06.sumOfNumbersN…M.js
@@ -1,11 +1,11 @@
-function sumOfNumbersNM(n, m){
-    let sum=0;
-    let firstNum=Number(n);
-    let secondNum=Number(m);
+function sumOfNumbersNM(n, m) {
+    let sum = 0;
+    let firstNum = Number(n);
+    let secondNum = Number(m);
 
-    for(let i=firstNum;i<=secondNum;i++){
-sum+=i;
+    for (let i = firstNum; i <= secondNum; i++) {
+        sum += i;
     }
     console.log(sum);
 }
-sumOfNumbersNM('1', '5' )
+sumOfNumbersNM('1', '5')
