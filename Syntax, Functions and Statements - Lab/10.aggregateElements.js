dc32d9055058b94b7fd02e385cb59396921e4600
Donika <marchevadonika@gmail.com>
2023-04-18 15:18:25 +0300

    Create 10.aggregateElements.js

---
+++ Syntax, Functions and Statements - Lab/10.aggregateElements.js
@@ -0,0 +1,10 @@
+function aggregateElements(array){
+let sum=array.reduce((a,c)=>a+c,0);
+let newArr=array.map(x=>1/x);
+let inverseValueSum=newArr.reduce((a,c)=>a+c,0);
+
+console.log(sum);
+console.log(inverseValueSum);
+console.log(array.join(''))
+}
+aggregateElements([1, 2, 3])
