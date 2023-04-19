2e855f6bfdff5cc0f69a5161eb864efd874a908f
Donika <marchevadonika@gmail.com>
2023-04-19 14:29:34 +0300

    .

--- Syntax, Functions and Statements - Exercise/03.sameNumbers.js
+++ Syntax, Functions and Statements - Exercise/03.sameNumbers.js
@@ -1,17 +1,16 @@
-function sameNumbers(num){
-    let numToString=num.toString();
-    let sum=numToString.split('').reduce((a,c)=>Number(a)+Number(c),0);
-
-    let isSame=true;
-    for(let i=0;i<numToString.length-1;i++){
-        let firstDigit=Number(numToString[i]);
-        let secondDigit=Number(numToString[i+1]);
-        if(firstDigit !== secondDigit){
-            isSame=false;
+function sameNumbers(num) {
+    let numToString = num.toString();
+    let sum = numToString.split('').reduce((a, c) => Number(a) + Number(c), 0);
+    let isSame = true;
+    for (let i = 0; i < numToString.length - 1; i++) {
+        let firstDigit = Number(numToString[i]);
+        let secondDigit = Number(numToString[i + 1]);
+        if (firstDigit !== secondDigit) {
+            isSame = false;
             break;
         }
     }
-    console.log(isSame===true?"true":"false");
+    console.log(isSame === true ? "true" : "false");
     console.log(sum);
 }
 sameNumbers(1234)
