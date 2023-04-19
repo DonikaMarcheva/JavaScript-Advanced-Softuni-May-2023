9a8b7ab580c7cae4f2b4dc088b8be664ff704617
Donika <marchevadonika@gmail.com>
2023-04-19 13:49:03 +0300

    Create 02.greatestCommonDivisor–GCD.js

---
+++ Syntax, Functions and Statements - Exercise/02.greatestCommonDivisor–GCD.js
@@ -0,0 +1,12 @@
+function greatestCommonDivisor(firstNum,secondNum){
+let divisor=1;
+let borderNum=Math.min(firstNum,secondNum);
+
+for(let i=2;i<=borderNum;i++){
+if(firstNum%i===0 && secondNum%i===0){
+    divisor=i;
+}
+}
+console.log(divisor);
+}
+greatestCommonDivisor(2154, 458)
