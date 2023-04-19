a05b3703eecd873bb275df2d65badb66638eca5d
Donika <marchevadonika@gmail.com>
2023-04-19 16:33:16 +0300

    .

--- Syntax, Functions and Statements - Exercise/05.timeToWalk.js
+++ Syntax, Functions and Statements - Exercise/05.timeToWalk.js
@@ -4,16 +4,16 @@ function timeToWalk(steps, footLength, speed) {
     let breakTime = Math.trunc(distanceInMeters / 500);
     let walkTime = (distanceInKm / speed);
     let hours = Math.trunc(walkTime);
-    if(hours<10){
-        hours="0"+hours;
+    if (hours < 10) {
+        hours = "0" + hours;
     }
     let minutes = Math.trunc(walkTime * 60 + breakTime);
-    if(minutes<10){
-        minutes="0"+minutes;
+    if (minutes < 10) {
+        minutes = "0" + minutes;
     }
     let seconds = Math.round(walkTime * 3600 - (Math.trunc(walkTime * 60)) * 60);
-    if(seconds<10){
-        seconds="0"+seconds;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
     }
     console.log(`${hours}:${minutes}:${seconds}`);
 }
---
+++ Syntax, Functions and Statements - Exercise/06.roadRadar.js
@@ -0,0 +1,26 @@
+function roadRadar(speed, area) {
+    let limit = 0;
+    // •	On the motorway, the limit is 130 km/h
+    // •	On the interstate, the limit is 90 km/h
+    // •	In the city, the limit is 50 km/h 
+    // •	Within a residential area, the limit is 20 km/h
+    switch (area) {
+        case "motorway": limit = 130; break;
+        case "interstate": limit = 90; break;
+        case "city": limit = 50; break;
+        case "residential": limit = 20; break;
+    }
+    let exceededSpeed=speed-limit;
+    if(exceededSpeed<=0){
+        console.log(`Driving ${speed} km/h in a ${limit} zone`);
+    }else{
+        let status="reckless driving";
+        if(exceededSpeed<=20){
+            status="speeding";
+        }else if(exceededSpeed<=40){
+            status="excessive speeding";
+        }
+        console.log(`The speed is ${exceededSpeed} km/h faster than the allowed speed of ${limit} - ${status}`);
+    }
+}
+roadRadar(21, 'residential')
