552b164fbc56bad2309708f5e4e74fb499612559
Donika <marchevadonika@gmail.com>
2023-04-20 14:37:59 +0300

    add a new solution

--- Syntax, Functions and Statements - Exercise/09.wordsUppercase.js
+++ Syntax, Functions and Statements - Exercise/09.wordsUppercase.js
@@ -1,7 +1,7 @@
-function wordsUppercase(string){
-    let pattern=/[A-Za-z0-9]+/g;
-    let match=string.match(pattern);
-    let newString=match.join(', ').toUpperCase();
+function wordsUppercase(string) {
+    let pattern = /[A-Za-z0-9]+/g;
+    let match = string.match(pattern);
+    let newString = match.join(', ').toUpperCase();
     console.log(newString);
 }
 wordsUppercase('hello')
