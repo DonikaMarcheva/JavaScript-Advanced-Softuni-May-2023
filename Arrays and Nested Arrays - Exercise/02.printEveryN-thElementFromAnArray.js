9c0652af13a4defabf2caa3368f03edb374d742b
Donika <marchevadonika@gmail.com>
2023-04-24 15:26:06 +0300

    Create 02.printEveryN-thElementFromAnArray.js

---
+++ Arrays and Nested Arrays - Exercise/02.printEveryN-thElementFromAnArray.js
@@ -0,0 +1,13 @@
+function printElement(array,step){
+    let newArr=[];
+for(let i=0;i<array.length;i+=step){
+    newArr.push(array[i]);
+}
+console.log(newArr);
+}
+printElement(['5',
+'20',
+'31',
+'4',
+'20'],
+2)
