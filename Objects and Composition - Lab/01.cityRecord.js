f07aa2b9d8c4d3606db02fa4d520d8976d579c00
Donika <marchevadonika@gmail.com>
2023-04-28 16:14:34 +0300

    Create 01.cityRecord.js

---
+++ Objects and Composition - Lab/01.cityRecord.js
@@ -0,0 +1,11 @@
+function cityrecord(name, population, treasury) {
+    let city = {
+        name,
+        population,
+        treasury
+    }
+    return city;
+}
+cityrecord('Tortuga',
+    7000,
+    15000)
