7785d4e507dcdfd1ae3856d8819e9671935a82bb
Donika <marchevadonika@gmail.com>
2023-05-12 15:08:00 +0300

    Create app.js

---
+++ 05.DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -0,0 +1,9 @@
+function addItem() {
+    let textAdded=document.getElementById('newItemText');
+    let itemsElement=document.getElementById('items')
+    let newElLi=document.createElement('li');
+    newElLi.textContent=textAdded.value;
+    itemsElement.appendChild(newElLi);
+
+
+}
