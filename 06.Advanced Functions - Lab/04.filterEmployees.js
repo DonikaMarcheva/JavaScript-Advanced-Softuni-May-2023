371239c7c6c247592d7152e7a540f3179db99291
Donika <marchevadonika@gmail.com>
2023-05-22 19:33:55 +0300

    Create 04.filterEmployees.js

---
+++ 06.Advanced Functions - Lab/04.filterEmployees.js
@@ -0,0 +1,31 @@
+function solve(string, criteria) {
+    let [property, value] = criteria.split('-');
+    let employee = JSON.parse(string);
+    let result = employee
+        .filter(x => x[property] == value)
+        .map((x, i) => `${i}. ${x.first_name} ${x.last_name} - ${x.email}`);
+    result.forEach(element => {
+        console.log(element);
+    });
+}
+solve(`[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+    }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+    }, 
+   {
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+    }]`,
+    'gender-Female')
