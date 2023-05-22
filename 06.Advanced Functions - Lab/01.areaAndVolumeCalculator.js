fce5a259f4ccce146dcbdd1663c1e933019fde37
Donika <marchevadonika@gmail.com>
2023-05-22 19:33:13 +0300

    Create 01.areaAndVolumeCalculator.js

---
+++ 06.Advanced Functions - Lab/01.areaAndVolumeCalculator.js
@@ -0,0 +1,23 @@
+function area() {
+    return Math.abs(this.x * this.y);
+};
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+function solve(area, vol, input) {
+    let inputArr = JSON.parse(input);
+    let result = [];
+    for (const points of inputArr) {
+        result.push({
+            area: area.call(points),
+            volume: vol.call(points)
+        })
+    }
+    return result;
+}
+solve(area, vol, `[
+{"x":"1","y":"2","z":"10"},
+{"x":"7","y":"7","z":"10"},
+{"x":"5","y":"2","z":"10"}
+]`
+)
