0f09ca99f76953a39775a9d66af12d15535bba32
Donika <marchevadonika@gmail.com>
2023-05-26 14:32:15 +0300

    Create app.js

---
+++ 07.Unit Testing and Error Handling - Exercise/05.Notification/app.js
@@ -0,0 +1,8 @@
+function notify(message) {
+  let divElement = document.getElementById('notification');
+  divElement.textContent = message;
+  divElement.style.display = 'block';
+  divElement.addEventListener('click', () => {
+    divElement.style.display = 'none';
+  })
+}
